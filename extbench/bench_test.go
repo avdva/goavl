0aa1d291b098f628bf46066e954c0d378adef85a
Aleksandr Demakin <alexander.demakin@gmail.com>
2023-03-10 17:54:11 +0000

    extbench: some improvements.

--- extbench/bench_test.go
+++ extbench/bench_test.go
@@ -19,7 +19,7 @@ func intCmp(a, b int) int {
 	return 0
 }
 
-func BenchmarkTidwallBTree(b *testing.B) {
+func BenchmarkTidwallBTreeInsert(b *testing.B) {
 	type item struct {
 		k, v int
 	}
@@ -29,83 +29,148 @@ func BenchmarkTidwallBTree(b *testing.B) {
 	}, btree.Options{
 		NoLocks: true,
 	})
-	keys := make([]int, b.N)
 	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
+		k := r.Int()
+		it := item{
+			k: k,
+			v: k,
+		}
+		t.Set(it)
+	}
+	if t.Len() == 0 {
+		panic("empty")
+	}
+}
+
+func BenchmarkKaraskAVLInsert(b *testing.B) {
+	tree := gavl.AVLTree{}
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		tree.Add(k, k)
+	}
+}
+
+func BenchmarkAVLInsert(b *testing.B) {
+	tree := goavl.New[int, int](intCmp)
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		tree.Insert(k, k)
+	}
+	if tree.Len() == 0 {
+		panic("empty")
+	}
+}
+
+func BenchmarkGoMapInsert(b *testing.B) {
+	m := make(map[int]int)
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		m[k] = k
+	}
+	if len(m) == 0 {
+		panic("empty")
+	}
+}
+
+func BenchmarkTidwallBTreeFind(b *testing.B) {
+	type item struct {
+		k, v int
+	}
+	b.StopTimer()
+	r := rand.New(rand.NewSource(0))
+	t := btree.NewBTreeGOptions(func(a, b item) bool {
+		return a.k < b.k
+	}, btree.Options{
+		NoLocks: true,
+	})
+	count := 250000
+	keys := make([]int, count)
+	for i := 0; i < count; i++ {
+		k := r.Int()
 		keys[i] = k
 		it := item{
 			k: k,
 			v: k,
 		}
 		t.Set(it)
-		if _, found := t.Get(it); !found {
-			panic("not found")
-		}
 	}
+	b.StartTimer()
+	var sum int
 	for i := 0; i < b.N; i++ {
-		it := item{
-			k: keys[i],
-		}
-		t.Delete(it)
+		toFind := keys[i%len(keys)]
+		it, _ := t.Get(item{
+			k: toFind,
+		})
+		sum += it.v
 	}
-	if t.Len() != 0 {
-		panic("not empty")
-	}
+	b.Logf("sum = %d", sum)
 }
 
-func BenchmarkKaraskAVL(b *testing.B) {
-	tree := gavl.AVLTree{}
-	r := rand.New(rand.NewSource(0))
-	keys := make([]int, b.N)
-	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
-		keys[i] = k
-		tree.Add(k, k)
-		if n := tree.Search(k); n == nil {
-			panic("not found")
-		}
-	}
-	for i := 0; i < b.N; i++ {
-		tree.Remove(keys[i])
-	}
-}
-
-func BenchmarkAVL(b *testing.B) {
+func BenchmarkAVLFind(b *testing.B) {
+	b.StopTimer()
 	tree := goavl.New[int, int](intCmp)
 	r := rand.New(rand.NewSource(0))
-	keys := make([]int, b.N)
-	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
+	count := 250000
+	keys := make([]int, count)
+	for i := 0; i < count; i++ {
+		k := r.Int()
 		keys[i] = k
 		tree.Insert(k, k)
-		if _, found := tree.Find(k); !found {
-			panic("not found")
-		}
 	}
+	b.StartTimer()
+	var sum int
 	for i := 0; i < b.N; i++ {
-		tree.Delete(keys[i])
+		toFind := keys[i%len(keys)]
+		v, _ := tree.Find(toFind)
+		sum += v
 	}
-	if tree.Len() != 0 {
-		panic("not empty")
-	}
+	b.Logf("sum = %d", sum)
 }
 
-func BenchmarkGoMap(b *testing.B) {
-	m := make(map[int]int)
+func BenchmarkTidwallBTreeAt(b *testing.B) {
+	type item struct {
+		k, v int
+	}
+	b.StopTimer()
 	r := rand.New(rand.NewSource(0))
-	keys := make([]int, b.N)
+	t := btree.NewBTreeGOptions(func(a, b item) bool {
+		return a.k < b.k
+	}, btree.Options{
+		NoLocks: true,
+	})
 	for i := 0; i < b.N; i++ {
 		k := r.Intn(50000)
-		keys[i] = k
-		m[k] = k
-		if _, found := m[k]; !found {
-			panic("not found")
+		it := item{
+			k: k,
+			v: k,
 		}
+		t.Set(it)
 	}
+	b.StartTimer()
+	var sum int
 	for i := 0; i < b.N; i++ {
-		delete(m, keys[i])
+		it, _ := t.GetAt(r.Int() % t.Len())
+		sum += it.k
 	}
-	if len(m) != 0 {
-		panic("not empty")
+	b.Logf("sum = %d", sum)
+}
+
+func BenchmarkAVLAt(b *testing.B) {
+	tree := goavl.New[int, int](intCmp, goavl.WithCountChildren(true))
+	b.StopTimer()
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		k := r.Intn(50000)
+		tree.Insert(k, k)
 	}
+	var sum int
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		k, _ := tree.At(r.Int() % tree.Len())
+		sum += k
+	}
+	b.Logf("sum = %d", sum)
 }
--- extbench/go.mod
+++ extbench/go.mod
@@ -3,9 +3,11 @@ module bench
 go 1.20
 
 require (
-	github.com/avdva/goavl v0.2.0
+	github.com/avdva/goavl v0.3.1
 	github.com/karask/go-avltree v0.0.0-20210208113816-739fb7fa1601
 	github.com/tidwall/btree v1.6.0
 )
+
+replace github.com/avdva/goavl => ../
 
 require golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb // indirect
