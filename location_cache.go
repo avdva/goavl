110b122c74378261419669fba314d79b6c413f70
Aleksandr Demakin <alexander.demakin@gmail.com>
2024-09-10 00:00:55 +0100

    tree: fix gometalinter warnings.

--- location_cache.go
+++ location_cache.go
@@ -18,7 +18,7 @@ func newSimpleLocationCache[K, V any]() *simpleLocationCache[K, V] {
 	return &simpleLocationCache[K, V]{}
 }
 
-func (lc *simpleLocationCache[K, V]) new(k K, v V) ptrLocation[K, V] {
+func (lc *simpleLocationCache[K, V]) new(k K, v V) ptrLocation[K, V] { //nolint:unused // used in locationCache iface
 	pn := &ptrNode[K, V]{}
 	pn.init(k, v)
 	return ptrLocation[K, V]{
@@ -30,7 +30,7 @@ type pooledLocationCache[K, V any] struct {
 	p sync.Pool
 }
 
-func (lc *simpleLocationCache[K, V]) release(ptrLocation[K, V]) {}
+func (lc *simpleLocationCache[K, V]) release(ptrLocation[K, V]) {} //nolint:unused // used in locationCache iface
 
 func newPooledLocationCache[K, V any]() *pooledLocationCache[K, V] {
 	return &pooledLocationCache[K, V]{
@@ -42,7 +42,7 @@ func newPooledLocationCache[K, V any]() *pooledLocationCache[K, V] {
 	}
 }
 
-func (lc *pooledLocationCache[K, V]) new(k K, v V) ptrLocation[K, V] {
+func (lc *pooledLocationCache[K, V]) new(k K, v V) ptrLocation[K, V] { //nolint:unused // used in locationCache iface
 	pn, _ := lc.p.Get().(*ptrNode[K, V])
 	pn.init(k, v)
 	return ptrLocation[K, V]{
@@ -50,6 +50,6 @@ func (lc *pooledLocationCache[K, V]) new(k K, v V) ptrLocation[K, V] {
 	}
 }
 
-func (lc *pooledLocationCache[K, V]) release(loc ptrLocation[K, V]) {
+func (lc *pooledLocationCache[K, V]) release(loc ptrLocation[K, V]) { //nolint:unused // used in locationCache iface
 	lc.p.Put(loc.ptrNode)
 }
