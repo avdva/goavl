02b215dfd63637fdfe53292d55c3218b70ec66b7
Aleksandr Demakin <alexander.demakin@gmail.com>
2024-09-29 17:30:16 +0100

    extbench: update benchmarked modules. (#12)

--- extbench/bench_test.go
+++ extbench/bench_test.go
@@ -9,17 +9,7 @@ import (
 	"github.com/tidwall/btree"
 )
 
-func intCmp(a, b int) int {
-	if a < b {
-		return -1
-	}
-	if a > b {
-		return 1
-	}
-	return 0
-}
-
-func BenchmarkTidwallBTree(b *testing.B) {
+func BenchmarkTidwallBTreeInsert(b *testing.B) {
 	type item struct {
 		k, v int
 	}
@@ -29,19 +19,74 @@ func BenchmarkTidwallBTree(b *testing.B) {
 	}, btree.Options{
 		NoLocks: true,
 	})
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		it := item{
+			k: k,
+			v: k,
+		}
+		t.Set(it)
+	}
+	b.Log(t.Len())
+}
+
+func BenchmarkTidwallBTreeFind(b *testing.B) {
+	type item struct {
+		k, v int
+	}
+	r := rand.New(rand.NewSource(0))
+	t := btree.NewBTreeGOptions(func(a, b item) bool {
+		return a.k < b.k
+	}, btree.Options{
+		NoLocks: true,
+	})
+	b.StopTimer()
 	keys := make([]int, b.N)
 	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
+		k := r.Int()
 		keys[i] = k
 		it := item{
 			k: k,
 			v: k,
 		}
 		t.Set(it)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		k := keys[i]
+		it := item{
+			k: k,
+			v: k,
+		}
 		if _, found := t.Get(it); !found {
 			panic("not found")
 		}
 	}
+	b.Log(t.Len())
+}
+
+func BenchmarkTidwallBTreeDelete(b *testing.B) {
+	type item struct {
+		k, v int
+	}
+	r := rand.New(rand.NewSource(0))
+	t := btree.NewBTreeGOptions(func(a, b item) bool {
+		return a.k < b.k
+	}, btree.Options{
+		NoLocks: true,
+	})
+	b.StopTimer()
+	keys := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		keys[i] = k
+		it := item{
+			k: k,
+			v: k,
+		}
+		t.Set(it)
+	}
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		it := item{
 			k: keys[i],
@@ -53,59 +98,117 @@ func BenchmarkTidwallBTree(b *testing.B) {
 	}
 }
 
-func BenchmarkKaraskAVL(b *testing.B) {
+func BenchmarkKaraskAVLInsert(b *testing.B) {
 	tree := gavl.AVLTree{}
 	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		tree.Add(k, k)
+	}
+}
+
+func BenchmarkKaraskAVLFind(b *testing.B) {
+	tree := gavl.AVLTree{}
+	r := rand.New(rand.NewSource(0))
+	b.StopTimer()
 	keys := make([]int, b.N)
 	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
+		k := r.Int()
 		keys[i] = k
 		tree.Add(k, k)
-		if n := tree.Search(k); n == nil {
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		if n := tree.Search(keys[i]); n == nil {
 			panic("not found")
 		}
 	}
+}
+
+func BenchmarkKaraskAVLDelete(b *testing.B) {
+	tree := gavl.AVLTree{}
+	r := rand.New(rand.NewSource(0))
+	b.StopTimer()
+	keys := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		keys[i] = k
+		tree.Add(k, k)
+	}
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		tree.Remove(keys[i])
 	}
 }
 
-func BenchmarkAVL(b *testing.B) {
-	tree := goavl.New[int, int](intCmp)
+func BenchmarkAVLInsert(b *testing.B) {
+	tree := goavl.New[int, int](func(a, b int) int {
+		if a < b {
+			return -1
+		}
+		if a > b {
+			return 1
+		}
+		return 0
+	})
+	r := rand.New(rand.NewSource(0))
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		tree.Insert(k, k)
+	}
+	b.Log(tree.Len())
+}
+
+func BenchmarkAVLFind(b *testing.B) {
+	tree := goavl.New[int, int](func(a, b int) int {
+		if a < b {
+			return -1
+		}
+		if a > b {
+			return 1
+		}
+		return 0
+	})
+	b.StopTimer()
 	r := rand.New(rand.NewSource(0))
 	keys := make([]int, b.N)
 	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
+		k := r.Int()
 		keys[i] = k
 		tree.Insert(k, k)
-		if _, found := tree.Find(k); !found {
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		if _, found := tree.Find(keys[i]); !found {
 			panic("not found")
 		}
 	}
+	b.Log(tree.Len())
+}
+
+func BenchmarkAVLDelete(b *testing.B) {
+	tree := goavl.New[int, int](func(a, b int) int {
+		if a < b {
+			return -1
+		}
+		if a > b {
+			return 1
+		}
+		return 0
+	})
+	b.StopTimer()
+	r := rand.New(rand.NewSource(0))
+	keys := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		k := r.Int()
+		keys[i] = k
+		tree.Insert(k, k)
+	}
+	b.StartTimer()
 	for i := 0; i < b.N; i++ {
 		tree.Delete(keys[i])
 	}
 	if tree.Len() != 0 {
-		panic("not empty")
-	}
-}
-
-func BenchmarkGoMap(b *testing.B) {
-	m := make(map[int]int)
-	r := rand.New(rand.NewSource(0))
-	keys := make([]int, b.N)
-	for i := 0; i < b.N; i++ {
-		k := r.Intn(50000)
-		keys[i] = k
-		m[k] = k
-		if _, found := m[k]; !found {
-			panic("not found")
-		}
-	}
-	for i := 0; i < b.N; i++ {
-		delete(m, keys[i])
-	}
-	if len(m) != 0 {
 		panic("not empty")
 	}
 }
